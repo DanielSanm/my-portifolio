3c89f727e55ceb9de3d0228cd6a8a456f91bc687
DanielSanm <danielelias3221@gmail.com>
2024-06-24 06:06:31 -0300

    starting coding

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            
+          
+        </a>
+      </header>
+    )
+}
+
+export default Header
